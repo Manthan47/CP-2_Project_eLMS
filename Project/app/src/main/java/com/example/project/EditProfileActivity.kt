660603212d1b5e45e50b47b5e535c20b1f03f531
Manthan Bhavsar <65906093+Manthan47@users.noreply.github.com>
2022-03-29 13:40:31 +0530

    6th Commit

---
+++ Project/app/src/main/java/com/example/project/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.project.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
